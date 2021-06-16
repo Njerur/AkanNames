dd2ed5d7376ba4a6c0e6de4b905a5ed5f5c26d08
Ryan Njeru <ryannjeru62@gmail.com>
2021-06-16 16:20:05 +0300

    Completed js.file

---
+++ js/script.js
@@ -0,0 +1,90 @@
+function naming() {
+    var CC=parseInt(document.getElementById("main").value);
+    var YY=parseInt(document.getElementById("name").value);
+    var MM=parseInt(document.getElementById("Form").value);
+    var DD=parseInt(document.getElementById("akan").value);
+
+    console.log(CC)
+
+    var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+    if(CC.value =="" || YY.value =="" || MM.value =="" || DD.value ==""){
+        alert("No blank values allowed");
+    }
+
+    var genders = document.querySelectorAll('input[name="gender"]')
+    var selectedValue;
+    for(var gender of genders){
+        if(gender.checked){
+            selectedValue=gender.value
+            break;
+        }
+    }
+    var  dayOfTheWeek = parseInt( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
+
+    console.log("here")
+    console.log(dayOfTheWeek);
+    console.log(gender)
+    if(selectedValue==="male") {
+      if(dayOfTheWeek===0){
+        alert("You were born on: " + maleNames[0]);
+        alert("Your Akan name is: " + maleNames[0]);
+    } 
+    if(dayOfTheWeek===1){
+      alert("You were born on: " + day[1]);
+      alert("Your Akan name is: " + maleNames[1]);
+    } 
+    if(dayOfTheWeek===2){
+        alert("You were born on: " + day[2]);
+        alert("Your Akan name is: " + maleNames[2]);
+    } 
+    if(dayOfTheWeek===3){
+        alert("You were born on: " + day[3]);
+        alert("Your Akan name is: " + maleNames[3]);
+    } 
+    if(dayOfTheWeek===4){
+        alert("You were born on: " + day[4]);
+        alert("Your Akan name is: " + maleNames[4]);
+    } 
+    if(dayOfTheWeek===5){
+        alert("You were born on: " + day[5]);
+        alert("Your Akan name is: " + maleNames[5]);
+    } 
+    if(dayOfTheWeek===6){
+        alert("You were born on: " + day[6]);
+        alert("Your Akan name is: " + maleNames[6]);
+    }
+
+  } else {
+     if(dayOfTheWeek===0){
+        alert("You were born on: " + femaleNames[0]);
+        alert("Your Akan name is: " + femaleNames[0]);
+    } 
+    if(dayOfTheWeek===1){
+      alert("You were born on: " + day[1]);
+      alert("Your Akan name is: " + femaleNames[1]);
+    } 
+    if(dayOfTheWeek===2){
+        alert("You were born on: " + day[2]);
+        alert("Your Akan name is: " + femaleNames[2]);
+    } 
+    if(dayOfTheWeek===3){
+        alert("You were born on: " + day[3]);
+        alert("Your Akan name is: " + femaleNames[3]);
+    } 
+    if(dayOfTheWeek===4){
+        alert("You were born on: " + day[4]);
+        alert("Your Akan name is: " + femaleNames[4]);
+    } 
+    if(dayOfTheWeek===5){
+        alert("You were born on: " + day[5]);
+        alert("Your Akan name is: " + femaleNames[5]);
+    } 
+    if(dayOfTheWeek===6){
+        alert("You were born on: " + day[6]);
+        alert("Your Akan name is: " + femaleNames[6]);
+    }
+  }
+}
